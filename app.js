b1e98b7fcb2403a34f7daac5d49e295107e878a9
Ivan Garcia <ivanvlora@gmail.com>
2018-10-12 17:35:50 +0900

    added mlab dev database

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var express             = require("express"),
-    bodyParser          = require("body-parser"),
-    methodOverride      = require("method-override"),
-    mongoose            = require("mongoose"),
-    app                 = express(),
-    seedDB              = require("./seeds"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
-    User                = require("./models/user"),
-    Comment             = require("./models/comment"),
-    Blog                = require("./models/blog"),
-    flash               = require("connect-flash"),
-    enforce             = require("express-sslify");
-    
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose"),
+  app = express(),
+  seedDB = require("./seeds"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user"),
+  Comment = require("./models/comment"),
+  Blog = require("./models/blog"),
+  flash = require("connect-flash"),
+  enforce = require("express-sslify");
+
 var commentRoutes = require("./routes/comments"),
-    blogRoutes    = require("./routes/blogs"),
-    indexRoutes    = require("./routes/index");
+  blogRoutes = require("./routes/blogs"),
+  indexRoutes = require("./routes/index");
 
 // APP CONFIG
-app.locals.moment   = require("moment");
-var url = process.env.DATABASEURL || "mongodb://localhost/english-teaching-blog";
+app.locals.moment = require("moment");
+var url = process.env.DATABASEURL || process.env.DEVDATABASE;
 mongoose.connect(url);
 app.set("view engine", "ejs");
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+//app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -29,29 +29,33 @@ app.use(flash());
 //seedDB();
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Time to learn English!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   res.locals.noMatch = req.flash("noMatch");
-   next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  res.locals.noMatch = req.flash("noMatch");
+  next();
 });
 
 app.use(indexRoutes);
 app.use(blogRoutes);
 app.use(commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("SERVER IS RUNNING");
+const port = process.env.PORT || 8080;
+
+app.listen(port, process.env.IP, function() {
+  console.log("SERVER IS RUNNING");
 });
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var Comment = require("../models/comment");
 var async = require("async");
 var nodemailer = require("nodemailer");
 var crypto = require("crypto");
-var dotenv = require('dotenv').config();
+var dotenv = require("dotenv").config();
 
 //=======================
 // AUTHENTICATON ROUTES
@@ -49,7 +49,7 @@ router.post("/login", function(req, res, next) {
     successRedirect: "/blogs",
     failureRedirect: "/login",
     failureFlash: true,
-    successflash: "Welcome to Ivan's Blog, " + req.body.username + "!"
+    successFlash: "Welcome back, " + req.body.username + "!"
   })(req, res);
 });
 
@@ -66,138 +66,181 @@ router.get("/users/:id", function(req, res) {
       req.flash("error", "Something went wrong.");
       res.redirect("/");
     }
-    Blog.find().where('author.id').equals(foundUser._id).exec(function(err, blogPosts) {
-      if (err) {
-        req.flash("error", "Something went wrong.");
-        res.redirect("/");
-      }
-      Comment.find().where('author.id').equals(foundUser.id).exec(function(err, comments) {
+    Blog.find()
+      .where("author.id")
+      .equals(foundUser._id)
+      .exec(function(err, blogPosts) {
         if (err) {
           req.flash("error", "Something went wrong.");
           res.redirect("/");
         }
-        res.render("users/show", { user: foundUser, blogPosts: blogPosts, comments: comments });
+        Comment.find()
+          .where("author.id")
+          .equals(foundUser.id)
+          .exec(function(err, comments) {
+            if (err) {
+              req.flash("error", "Something went wrong.");
+              res.redirect("/");
+            }
+            res.render("users/show", {
+              user: foundUser,
+              blogPosts: blogPosts,
+              comments: comments
+            });
+          });
       });
-    });
   });
 });
 
 // forgot password
-router.get('/forgot', function(req, res) {
-  res.render('forgot');
+router.get("/forgot", function(req, res) {
+  res.render("forgot");
 });
 
-router.post('/forgot', function(req, res, next) {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'No account with that email address exists.');
-          return res.redirect('/forgot');
-        }
-
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-        user.save(function(err) {
-          done(err, token, user);
+router.post("/forgot", function(req, res, next) {
+  async.waterfall(
+    [
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString("hex");
+          done(err, token);
         });
-      });
-    },
-    function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: 'ivangarcia@flowgengo.com',
-          pass: process.env.GMAILPW
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'ivangarcia@flowgengo.com',
-        subject: 'Password Reset For Ivans Blog',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'https://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log('mail sent');
-        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    res.redirect('/forgot');
-  });
-});
+      },
+      function(token, done) {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (!user) {
+            req.flash("error", "No account with that email address exists.");
+            return res.redirect("/forgot");
+          }
 
-router.get('/reset/:token', function(req, res) {
-  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-    if (!user) {
-      req.flash('error', 'Password reset token is invalid or has expired.');
-      return res.redirect('/forgot');
-    }
-    res.render('reset', { token: req.params.token });
-  });
-});
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-router.post('/reset/:token', function(req, res) {
-  async.waterfall([
-    function(done) {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'Password reset token is invalid or has expired.');
-          return res.redirect('back');
-        }
-        if (req.body.password === req.body.confirm) {
-          user.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-
-            user.save(function(err) {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
+          user.save(function(err) {
+            done(err, token, user);
           });
-        }
-        else {
-          req.flash("error", "Passwords do not match.");
-          return res.redirect('back');
-        }
-      });
-    },
-    function(user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: 'ivangarcia@flowgengo.com',
-          pass: process.env.GMAILPW
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'ivangarcia@flowgengo.com',
-        subject: 'Your password has been changed',
-        text: 'Hello,\n\n' +
-          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        req.flash('success', 'Success! Your password has been changed.');
-        done(err);
-      });
+        });
+      },
+      function(token, user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "ivangarcia@flowgengo.com",
+            pass: process.env.GMAILPW
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: "ivangarcia@flowgengo.com",
+          subject: "Password Reset For Ivans Blog",
+          text:
+            "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+            "https://" +
+            req.headers.host +
+            "/reset/" +
+            token +
+            "\n\n" +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          console.log("mail sent");
+          req.flash(
+            "success",
+            "An e-mail has been sent to " +
+              user.email +
+              " with further instructions."
+          );
+          done(err, "done");
+        });
+      }
+    ],
+    function(err) {
+      if (err) return next(err);
+      res.redirect("/forgot");
     }
-  ], function(err) {
-    res.redirect('/blogs');
-  });
+  );
+});
+
+router.get("/reset/:token", function(req, res) {
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() }
+    },
+    function(err, user) {
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+      }
+      res.render("reset", { token: req.params.token });
+    }
+  );
+});
+
+router.post("/reset/:token", function(req, res) {
+  async.waterfall(
+    [
+      function(done) {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() }
+          },
+          function(err, user) {
+            if (!user) {
+              req.flash(
+                "error",
+                "Password reset token is invalid or has expired."
+              );
+              return res.redirect("back");
+            }
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function(err) {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function(err) {
+                  req.logIn(user, function(err) {
+                    done(err, user);
+                  });
+                });
+              });
+            } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+      function(user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "ivangarcia@flowgengo.com",
+            pass: process.env.GMAILPW
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: "ivangarcia@flowgengo.com",
+          subject: "Your password has been changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.email +
+            " has just been changed.\n"
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash("success", "Success! Your password has been changed.");
+          done(err);
+        });
+      }
+    ],
+    function(err) {
+      res.redirect("/blogs");
+    }
+  );
 });
 
 module.exports = router;
